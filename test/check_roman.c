b1b7515cb2cdea93ebb1c81ed33ccbf14d8bfd59
Zack Brown <zbrown@pillartechnology.com>
2016-07-30 16:00:06 -0400

    test for converting Xs to 10s

--- test/check_roman.c
+++ test/check_roman.c
@@ -21,6 +21,19 @@ START_TEST(it_converts_V_to_5)
  }
 END_TEST
 
+START_TEST(it_converts_X_to_10)
+ {
+   ck_assert_int_eq(convert_to_arabic("X"), 10);
+ }
+END_TEST
+
+
+START_TEST(it_converts_XX_to_20)
+ {
+   ck_assert_int_eq(convert_to_arabic("X"), 10);
+ }
+END_TEST
+
 Suite * convert_suite(void)
 {
   Suite *s;
@@ -33,6 +46,8 @@ Suite * convert_suite(void)
   tcase_add_test(tc_core, it_converts_I_to_1);
   tcase_add_test(tc_core, it_converts_II_to_2);
   tcase_add_test(tc_core, it_converts_V_to_5);
+  tcase_add_test(tc_core, it_converts_X_to_10);
+  tcase_add_test(tc_core, it_converts_XX_to_20);
   suite_add_tcase(s, tc_core);
 
   return s;
