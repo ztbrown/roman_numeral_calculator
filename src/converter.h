0e018b836aa9ab5f7402240e09646b48e8794e0d
Zack Brown <zbrown@pillartechnology.com>
2016-07-30 16:25:43 -0400

    bring in helper functions from previous RN kata attempt, rewrite IV to IIII

--- src/converter.h
+++ src/converter.h
@@ -2,5 +2,7 @@
 #define CONVERTER_H
 
 int convert_to_arabic(char *roman_numeral);
+void gsub(char *str, const char *pattern, const char *repl);
+void rewrite_to_ignore_subtraction_rules(char *roman_numeral);
 
 #endif
