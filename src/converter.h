48da2939b698a640976bfe5ea2b7a273959e08a3
Zack Brown <zbrown@pillartechnology.com>
2016-08-03 12:20:04 -0400

    internally used functions should not be defined within the header and should be defined 'static'

--- src/converter.h
+++ src/converter.h
@@ -5,15 +5,4 @@
 int convert_to_arabic(const char *number);
 char * convert_from_arabic(int number);
 
-// internal functions
-void gsub(char *str, const char *pattern, const char *repl);
-void rewrite_to_ignore_subtraction_rules(char *roman_numeral);
-void extract(char **numeral, char nums[2], int num_size);
-void add_to_buffer(char nums[2], int size, char ** ptr);
-
-struct conversion_table {
-    char arr[2];
-    size_t len;
-};
-
 #endif
