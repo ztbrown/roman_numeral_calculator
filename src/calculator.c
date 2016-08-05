1b518789bc038d15e1d505b1fedb05ea2872ec5e
Zack Brown <zbrown@pillartechnology.com>
2016-08-04 21:53:58 -0400

    pass validation test

--- src/calculator.c
+++ src/calculator.c
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 #include "converter.h"
 #include "calculator.h"
+#include "validator.h"
 
-char * add(const char *a, const char *b, char * result) {
-  return convert_from_arabic(convert_to_arabic(a) + convert_to_arabic(b), result);
+char * add(const char *a, const char *b, char * result)
+{
+  if (validate(a) == 0 && validate(b) == 0){
+    convert_from_arabic(convert_to_arabic(a) + convert_to_arabic(b), result);
+    if (validate(result) == 1 ){
+      result = "Invalid Entry";
+    }
+  }
+
+  return result;
 }
 
 char * subtract(const char *a, const char *b, char * result) {
