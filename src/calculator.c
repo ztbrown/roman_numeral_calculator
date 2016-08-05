2762e30d01e0e9818ea6212c6ed1b2e3cdd8c2fc
Zack Brown <zbrown@pillartechnology.com>
2016-08-04 22:02:33 -0400

    pass subtraction validation tests

--- src/calculator.c
+++ src/calculator.c
@@ -16,5 +16,12 @@ char * add(const char *a, const char *b, char * result)
 }
 
 char * subtract(const char *a, const char *b, char * result) {
-  return convert_from_arabic(convert_to_arabic(a) - convert_to_arabic(b), result);
+  if (validate(a) == 0 && validate(b) == 0){
+    convert_from_arabic(convert_to_arabic(a) - convert_to_arabic(b), result);
+    if (validate(result) == 1 ){
+      result = "Invalid Entry";
+    }
+  }
+
+  return result;
 }
