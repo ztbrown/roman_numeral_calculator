033253e128c5cb9cdd3ef4846ad18c5ad1a29c7c
Zack Brown <zbrown@pillartechnology.com>
2016-08-04 22:00:56 -0400

    failing validation test

--- test/check_validator.c
+++ test/check_validator.c
@@ -141,6 +141,12 @@ START_TEST(it_validates_D_and_returns_success)
 }
 END_TEST
 
+START_TEST(it_validates_empty_string_and_returns_failure)
+{
+  ck_assert_int_eq(validate(""), 1);
+}
+END_TEST
+
 Suite * validator_suite(void)
 {
   Suite *s;
@@ -173,6 +179,7 @@ Suite * validator_suite(void)
   tcase_add_test(tc_core, it_validates_L_and_returns_success);
   tcase_add_test(tc_core, it_validates_LX_and_returns_success);
   tcase_add_test(tc_core, it_validates_D_and_returns_success);
+  tcase_add_test(tc_core, it_validates_empty_string_and_returns_failure);
 
   suite_add_tcase(s, tc_core);
 
