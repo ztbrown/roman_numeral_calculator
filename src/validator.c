e55a1ee681c716130862dd37ebc6ad1a79788724
Zack Brown <zbrown@pillartechnology.com>
2016-08-04 22:01:11 -0400

    passing validation test

--- src/validator.c
+++ src/validator.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <regex.h>
+#include <string.h>
 #include "validator.h"
 
 regex_t regex;
@@ -10,7 +11,7 @@ int validate(const char *a) {
   int return_value = 1;
 
   reti = regcomp(&regex, "^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$", REG_EXTENDED);
-  if (reti) {
+  if (reti || strlen(a) == 0) {
     return 1;
   }
 
